61782feb53e73fc9d422aed26b944d192e4217c1
Leighton Nguyen <92602519+TienNguyen4101@users.noreply.github.com>
2024-01-31 11:44:54 +0700

    Update login_server.php

--- happy_new_year/handle_logic/login_server.php
+++ happy_new_year/handle_logic/login_server.php
@@ -1,25 +1,22 @@
 <?php
-	$server_BK = "103.112.209.137";
-	$user_BK = "User237";
-	$password_BK = "AdminDigitel@12312323";
+	$server_BK = "";
+	$user_BK = "";
+	$password_BK = "";
 	$db_BK = "VoiceReport";
 
-	$server_main = "103.112.209.136";
-	$user_main = "User237";
-	$password_main = "AdminDigitel@12312323";
+	$server_main = "";
+	$user_main = "";
+	$password_main = "";
 	$db_main = "VoiceReport";
 	
-	$server_local = "localhost";
-	$username_local = "User99";
-	$password_local = "Chiru@123";
+	$server_local = "";
+	$username_local = "";
+	$password_local = "";
 	$db_in_local = "VoiceRecord";
 
-	$apiToken = "6369084280:AAFxZhzm4CrUznvj7ZAENCznn3AR-JKqg7M";
-	$chat_id = "@test_message123";
-
 	//CHANGEME
-	$username_email = "tienxemutube412001@gmail.com";
-	$password_email = "bmxpcpdvnbdyvolt"; 
+	$username_email = "@gmail.com";
+	$password_email = ""; 
 	$subject_email = "[DIGITEL] - THÔNG BÁO LỊCH NGHỈ LỄ TẾT ÂM LỊCH 2024";
 	$from_name_email = 'CÔNG TY CỔ PHẦN HẠ TẦNG VIỄN THÔNG SỐ';
 	//=====================================================================
